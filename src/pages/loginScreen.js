27a279802fa3784ef30e4f0a82a8fe96f4010595
William Gyasi <kwabenagyasi63@gmail.com>
2020-03-21 04:51:10 -0700

    First Login Logic

--- src/pages/loginScreen.js
+++ src/pages/loginScreen.js
@@ -9,9 +9,53 @@ import MaterialsIcon from 'react-native-vector-icons/MaterialIcons';
 import { Sae,Kohana } from 'react-native-textinput-effects';
 import SwiperFlatList from 'react-native-swiper-flatlist';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import AnimateLoadingButton from 'react-native-animate-loading-button';
 import FontAwesome5Icon from 'react-native-vector-icons/FontAwesome5';
 
 export default class LoginScreen extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={email:'',password:''}
+        this.state={borderColor:'#4a7556'}
+        this.shakeAnimation = new Animated.Value(0);
+
+    }
+
+    startShake = () => {
+        Animated.sequence([
+          Animated.timing(this.shakeAnimation, { toValue: 10, duration: 100, useNativeDriver: true }),
+          Animated.timing(this.shakeAnimation, { toValue: -10, duration: 100, useNativeDriver: true }),
+          Animated.timing(this.shakeAnimation, { toValue: 10, duration: 100, useNativeDriver: true }),
+          Animated.timing(this.shakeAnimation, { toValue: 0, duration: 100, useNativeDriver: true })
+        ]).start();
+     }
+
+    handleLogin=()=>{
+        this.loadingButton.showLoading(true)
+        const {email,password}=this.state
+        try {
+            if (email=="Test@gmail.com" && password=="Test123"){
+               this.setState({
+                borderColor:'#4a7556'
+               })
+                alert('sucess')
+                this.loadingButton.showLoading(false)     
+            }
+            else{
+                this.loadingButton.showLoading(false)
+                this.setState({
+                    borderColor:'red'
+                })
+            }
+        } catch (error) {
+            setTimeout(()=>{
+                this.loadingButton.showLoading(false) 
+            },100)  
+            
+        }
+     
+        
+    }
     render(){
         return(
           <Container style={{}}>
@@ -20,15 +64,18 @@ export default class LoginScreen extends React.Component{
                     LOGIN
                   </Text>
               </View>
-            
-
+    
               <View style={{flexDirection:'column',height:150,marginHorizontal:30,marginTop:40}}>
               <Kohana
-    style={{marginHorizontal:20,backgroundColor:'',borderRadius:20,borderWidth:3,borderColor:'#4a7556',}}
+    style={{marginHorizontal:20,backgroundColor:'',borderRadius:20,borderWidth:3,borderColor:this.state.borderColor,}}
     label={'Username'}
     iconClass={FontAwesomeIcon}
     iconName={'dog'}
     iconColor={'#41ba69'}
+    value={this.state.email}
+    onChangeText={(email)=>{
+        this.setState({email})
+    }}
     inputPadding={12}
     labelStyle={{ color: '#108f3a', }}
     inputStyle={{ color: '#a7b5ab' }}
@@ -36,11 +83,17 @@ export default class LoginScreen extends React.Component{
     iconContainerStyle={{ padding: 10 }} />  
 
     <Kohana
-     style={{marginHorizontal:20,backgroundColor:'',borderRadius:20,borderWidth:3,borderColor:'#4a7556',marginTop:20}}
+     style={{marginHorizontal:20,backgroundColor:'',borderRadius:20,borderWidth:3,borderColor:this.state.borderColor,marginTop:20}}
     label={'Password'}
     iconClass={FontAwesomeIcon}
     iconName={'paw'}
     iconColor={'#41ba69'}
+    value={this.state.password}
+    onChangeText={(password)=>{
+        this.setState({
+            password
+        })
+    }}
     secureTextEntry
     inputPadding={12}
     labelStyle={{ color: '#108f3a' }}
@@ -48,11 +101,24 @@ export default class LoginScreen extends React.Component{
     labelContainerStyle={{ padding: 10 }}
     iconContainerStyle={{ padding: 10 }} />                  
               </View>
-              <RaisedButton style={styles.loginButton}>
+              {/* <RaisedButton style={styles.loginButton}>
                   <Text style={{color:'white',}}>
                       Login
                   </Text>
-              </RaisedButton>
+              </RaisedButton> */}
+              <View style={{marginHorizontal:150,marginTop:20,elevation:20}}>
+              <AnimateLoadingButton
+          ref={c => (this.loadingButton = c)}
+          width={120}
+          height={50}
+          title="LOGIN"
+          titleFontSize={16}
+          titleColor="rgb(255,255,255)"
+          backgroundColor='green'
+          borderRadius={10}
+          onPress={this.handleLogin.bind(this)}
+        />
+        </View>
 
               <View style={{marginTop:15,flexDirection:'row'}}>
                   <View
